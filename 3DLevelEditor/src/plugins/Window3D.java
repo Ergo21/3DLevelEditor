7cd8f5ee7f4882df4b54b01289d52a5d1deee096
Ergo21 <jww14@uni.brighton.ac.uk>
2015-01-10 19:14:34 +0000

    Created simple lookAt() function for the camera, with orbit.

--- 3DLevelEditor/src/plugins/Window3D.java
+++ 3DLevelEditor/src/plugins/Window3D.java
@@ -3,13 +3,13 @@ package plugins;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.SubScene;
-import javafx.scene.input.InputEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -28,7 +28,10 @@ public class Window3D {
 	private Stage stage;
 	private SubScene subScene;
 	private PerspectiveCamera camera;
+	private Point3D rotPoi;
 	
+	private Point3D camXAx;
+	private Point3D camYAx;
 	private Rotate rotateX;
 	private Rotate rotateY;
 	
@@ -38,6 +41,9 @@ public class Window3D {
 	
 	public void pluginStart() {
 		
+		camXAx = new Point3D(1,0,0);
+		camYAx = new Point3D(0,1,0);
+		
 		stage = new Stage(StageStyle.UTILITY);
 		stage.setWidth(200);
 		stage.setHeight(200);
@@ -46,7 +52,8 @@ public class Window3D {
         
         scene.setOnKeyPressed(event->handleKeyboard(event));
         scene.setOnMouseMoved(event->handleMouseMove(event));
-        scene.setOnMouseClicked(event->handleMouseInput(event));;
+        scene.setOnMouseClicked(event->handleMouseInput(event));
+        scene.setOnMouseDragged(event->handleMouseMove(event));
         
         stage.setScene(scene);
         
@@ -100,9 +107,10 @@ public class Window3D {
         subScene.widthProperty().bind(stage.widthProperty());
         subScene.heightProperty().bind(stage.heightProperty());
         
-        rotateX = new Rotate(0, Rotate.X_AXIS);
-        //rotateY = new Rotate(0, 0,0,10, Rotate.Y_AXIS);
-        rotateY = new Rotate(0, Rotate.Y_AXIS);
+        rotateX = new Rotate(0, 0,0,10, camXAx);
+        rotateY = new Rotate(0, 0,0,10, camYAx);
+        //rotateX = new Rotate(0, Rotate.X_AXIS);
+        //rotateY = new Rotate(0, Rotate.Y_AXIS);
 
         return new Group(subScene);
     }
@@ -152,29 +160,22 @@ public class Window3D {
     }
     
     double curMX = 0;
-    double rotYA = 0;
 	double curMY = 0;
-	double rotXA = 0;
 	long timePaused = 0;
     public void handleMouseMove(MouseEvent me){  
     	double curChX = me.getSceneX() - curMX; 
 		curChX = curChX*(180/stage.widthProperty().doubleValue());
 		double curChY = me.getSceneY() - curMY;
 		curChY = curChY*(180/stage.widthProperty().doubleValue());
-    	if(me.isControlDown()) { 
-    		rotYA += curChX;
+    	if(me.isSecondaryButtonDown()) { 
     		camera.getTransforms().remove(rotateY);
-    		rotateY.setAngle(rotYA);  		
+    		rotateY.setAngle(rotateY.getAngle() + curChX);  		
     		camera.getTransforms().add(rotateY); 	
-    		rotXA += curChY;
     		camera.getTransforms().remove(rotateX);
-    		rotateX.setAngle(-rotXA);  	
+    		rotateX.setAngle(rotateX.getAngle() - curChY);  	
     		camera.getTransforms().add(rotateX); 	
-    		//rotateY.setAngle((curMouseX - me.getScreenX())/50);
-	       // camera.getTransforms().add(rotateY); 	
     	}
-    	else if(me.isShiftDown() && System.nanoTime() - timePaused > 20){
-    		timePaused = System.nanoTime();
+    	else if(me.isShiftDown()){
     	}
     	
     	
@@ -185,7 +186,52 @@ public class Window3D {
     public void handleMouseInput(MouseEvent me){
     	if(me.getTarget() != subScene){
     		System.out.println(me.getTarget().getClass());
+    		selectObject((Box)me.getTarget());
     	}    	
+    }
+    
+    public void selectObject(Box b){
+    	Point3D p = new Point3D(0, 0, 0);
+    	
+    	p = p.add(b.getTranslateX(), b.getTranslateY(), b.getTranslateZ());
+    	
+    	camera.getTransforms().remove(rotateX);
+    	camera.getTransforms().remove(rotateY);
+    	
+    	for(int i = 0; i < camera.getTransforms().size(); i++){
+    		p = p.add(-camera.getTransforms().get(i).getTx(), 
+    				  -camera.getTransforms().get(i).getTy(), 
+    				  -camera.getTransforms().get(i).getTz());
+    	}
+    	
+    	//lookAt(p);
+    	
+    	rotateX.setAngle(0);
+        rotateY.setAngle(0);
+        
+    	camera.getTransforms().add(rotateX);
+    	camera.getTransforms().add(rotateY);
+    	lookAt(p);
+    	
+    }
+    
+    Rotate lookAtX;
+    Rotate lookAtY;
+    
+    void lookAt(Point3D p){
+    	camera.getTransforms().remove(lookAtX);
+    	camera.getTransforms().remove(lookAtY);
+    	Point3D p2 = new Point3D(0,0,0);
+    	for(int i = 0; i < camera.getTransforms().size(); i++){
+    		p2 = p2.add(-camera.getTransforms().get(i).getTx(), 
+    				  -camera.getTransforms().get(i).getTy(), 
+    				  -camera.getTransforms().get(i).getTz());
+    	}
+    	
+    	lookAtX = new Rotate(Math.toDegrees(-Math.atan(p.getY()/p.getZ())), Rotate.X_AXIS);
+    	lookAtY = new Rotate(Math.toDegrees(Math.atan(p.getX()/p.getZ())), Rotate.Y_AXIS);
+    	camera.getTransforms().add(lookAtX);
+        camera.getTransforms().add(lookAtY);
     }
 
     class Cube extends Box {
