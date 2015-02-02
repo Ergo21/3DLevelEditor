00d3ffc32b852da5037c9080dc6f715322c3aa0e
Ergo21 <jww14@uni.brighton.ac.uk>
2015-02-02 15:02:41 +0000

    Detrimented TLEData in favour of javafx's Node. Shifted the Cube from Window3D to World. The Window3D will now refer to the World's "CurrentLevel" for data.

--- 3DLevelEditor/src/baseProgram/World.java
+++ 3DLevelEditor/src/baseProgram/World.java
@@ -3,13 +3,57 @@ package baseProgram;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
 import common.*;
 
 public class World {
 	MainWindow rootWindow;
-	HashMap<String, ArrayList<TLEData>> worldData;
+	HashMap<String, ArrayList<Node>> worldData;
 	
 	public World(MainWindow mW){
 		rootWindow = mW;
+		worldData = new HashMap<String, ArrayList<Node>>();
+		
+		Cube c = new Cube(1, Color.GREEN);
+        c.rx.setAngle(45);
+        c.ry.setAngle(45);
+
+        Cube c2 = new Cube(1, Color.BLUE);
+        c2.setTranslateX(2);
+        c2.rx.setAngle(45);
+        c2.ry.setAngle(45);
+
+        Cube c3 = new Cube(5, Color.RED);
+        c3.setTranslateX(-10);
+        c3.rx.setAngle(45);
+        c3.ry.setAngle(45);
+        
+        ArrayList<Node> cC = new ArrayList<Node>();
+        cC.add(c);
+        cC.add(c2);
+        cC.add(c3);
+        
+        worldData.put("CurrentLevel", cC);
 	}
+	
+	public HashMap<String, ArrayList<Node>> getData(){
+		return worldData; 
+	}
+	
+	class Cube extends Box {
+
+        final Rotate rx = new Rotate(0, Rotate.X_AXIS);
+        final Rotate ry = new Rotate(0, Rotate.Y_AXIS);
+        final Rotate rz = new Rotate(0, Rotate.Z_AXIS);
+
+        public Cube(double size, Color color) {
+            super(size, size, size);
+            setMaterial(new PhongMaterial(color));
+            getTransforms().addAll(rz, ry, rx);
+        }
+    }
 }
