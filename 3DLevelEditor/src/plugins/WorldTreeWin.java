973257b6b7e73b2fffadbe2982c0c80731281654
Ergo21 <jww14@uni.brighton.ac.uk>
2015-02-11 11:05:01 +0000

    Created basic Tree Window to show World data.

---
+++ 3DLevelEditor/src/plugins/WorldTreeWin.java
@@ -0,0 +1,84 @@
+package plugins;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import baseProgram.PluginManager;
+
+/**
+ * Creates a 3D Window to display the world.
+ * @author Ergo21
+ */
+
+public class WorldTreeWin {
+	private PluginManager pMRef;
+	private Stage stage;
+	TreeItem<String> rootItem;
+	
+	public WorldTreeWin(PluginManager pM) {
+		pMRef = pM;
+	}
+	
+	/**
+	 *  Sets up plugin with stage, camera, and listeners
+	 */
+	public void pluginStart() {
+		
+		stage = new Stage(StageStyle.UTILITY);
+		stage.setWidth(200);
+		stage.setHeight(400);
+		stage.setTitle("Yggdrasil");       
+		
+		pMRef.getWorld().addResetWindow("WindowTree", ()->resetWindow());
+	        
+	    rootItem = new TreeItem<String> ("World");
+	    rootItem.setExpanded(true);
+	    
+	    rootItem = createContent(rootItem);
+	    
+	    TreeView<String> tree = new TreeView<String> (rootItem);        
+	    StackPane root = new StackPane();
+	    root.getChildren().add(tree);
+	    Scene subScene = new Scene(root, 200, 400);
+	    stage.setScene(subScene);
+	    
+	    stage.show();
+	}
+	
+	public void pluginClose() {
+		stage.close();
+	}
+
+	/**
+	 * Creates all objects shown. Will be overwritten to load from Data when complete.
+	 * @return Scene to add to Stage
+	 */
+    public TreeItem<String> createContent(TreeItem<String> root) {
+        
+        HashMap<String, ArrayList<Node>> tWor = pMRef.getWorld().getData();
+        String[] keys = new String[1];
+        tWor.keySet().toArray(keys);		
+        
+        if(tWor != null){   	
+        	for(int i = 0; i < keys.length; i++){
+        		TreeItem<String> item = new TreeItem<String> (keys[i]);
+        		//tWor.get(keys[i]).get(0).
+        		root.getChildren().add(item);
+        	}
+        }
+
+        return root;
+    }    
+
+    public void resetWindow(){
+    	rootItem.getChildren().clear();
+    	rootItem = createContent(rootItem);
+    }
+}
---
+++ 3DLevelEditor/src/plugins/WorldTreeWinPlugin.java
@@ -0,0 +1,33 @@
+package plugins;
+
+import baseProgram.PluginManager;
+import common.*;
+
+/**
+ * This plugin displays the overall structure of the World as a TreeView.
+ * @author Ergo21
+ *
+ */
+
+public class WorldTreeWinPlugin extends TLEPlugin {
+
+	public WorldTreeWinPlugin(){
+		pluginName = "WorldTreeWinPlugin";
+	}
+	
+	/**
+	 * Adds new buttons under Plugins/3D Model Loader. 
+	 * 
+	 * @param pM	PluginManager for the Plugin to interact with the Base Program
+	 */
+	@Override
+	public void install(PluginManager pM) {
+		mainPlMan = pM;
+		mainPlMan.getMWin().addMenuBarItem(event -> showYggdrasil(), "Plugins", "Data Tree", "New Window");
+	}
+	
+	public void showYggdrasil(){
+		WorldTreeWin ygg = new WorldTreeWin(mainPlMan);
+    	ygg.pluginStart();
+	}
+}
